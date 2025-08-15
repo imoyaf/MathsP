e911838777e9a1154f3cbce8d25c2f47082f4bbb
Ignasi Moya <imoyadv@gmail.com>
2025-08-15 14:23:25 +0200

    Initial commit

---
+++ mathsprim/src/main/java/com/imf/mathsprim/MathsprimApplication.java
@@ -0,0 +1,13 @@
+package com.imf.mathsprim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathsprimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathsprimApplication.class, args);
+	}
+
+}
---
+++ mathsprim/src/test/java/com/imf/mathsprim/MathsprimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imf.mathsprim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathsprimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
